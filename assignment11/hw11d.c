b792823eccd286e38b96f2d0660bf7f8c440f86b
Sibgha Ahmad <sibghaahmad@login-el8.khoury.northeastern.edu>
2024-07-23 19:32:31 -0400

    submission

--- assignment11/hw11d.c
+++ assignment11/hw11d.c
@@ -1,5 +1,5 @@
-// name: <your name here>
-// email: <your email here>
+// name: Sibgha Ahmad
+// email: ahmad.si@northeastern.edu
 
 #include <stdio.h>
 
@@ -8,6 +8,14 @@ int d[20];
 long long int dp(int n) {
 
     // Add your code here
+    if(n == 0) return 1;
+    if(n == 1) return 1;
+    if(d[n] != 0) return d[n];
+    d[n] = 3 * dp(n - 2) + 2 * dp(n - 1);
+    for(int i = n - 3; i >= 0; i--){
+      d[n] += 2 * dp(i);
+    }
+    return d[n];
 
 }
 
